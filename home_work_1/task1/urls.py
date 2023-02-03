54dd632d79e1ab7f3eb9227df59b73b73af95ba5
Andrii Merkulov <117385170+HECTPUMHUI@users.noreply.github.com>
2023-02-04 01:38:06 +0200

    Update home_work_1/task1/urls.py
    change name for ""
    
    Co-authored-by: Serhii Horodilov <sgorodil@gmail.com>

--- home_work_1/task1/urls.py
+++ home_work_1/task1/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('homepage/', views.homepage, name='homepage'),
     path('home/', views.homepage, name='home'),
-    path('', views.homepage, name='slash'),
+    path('', views.homepage, name='homepage'),
     path('greeting/<str:name>', views.greeting, name='greeting'),
     path('fib/<int:n>', views.fib, name='fib'),
     path('progression/<int:start>/<int:count>/<int:step>', views.progression, name='progression'),
